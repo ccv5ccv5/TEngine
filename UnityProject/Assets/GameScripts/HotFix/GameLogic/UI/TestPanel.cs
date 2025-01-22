5e0decd0bf45b040bb31b485d7f56ae45e6aa21c
ccv5ccv5 <406877924@qq.com>
2025-01-22 17:10:47 +0800

    测试代码更新

--- UnityProject/Assets/GameScripts/HotFix/GameLogic/UI/TestPanel.cs
+++ UnityProject/Assets/GameScripts/HotFix/GameLogic/UI/TestPanel.cs
@@ -2,15 +2,34 @@ using System.Collections;
 using System.Collections.Generic;
 using TEngine;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class TestPanel : MonoBehaviour
 {
+    private int _count = 0;
     // Start is called before the first frame update
     void Start()
     {
         Log.Debug("TestPanel Start DO Hotfix");
+
+        _count = 0;
+
+        // _scoreLabel = this.transform.Find("Button (Legacy)").GetComponent<Text>();
+        // _scoreLabel.text = "Score : 0";
+
+        var restartBtn = this.transform.Find("Button (Legacy)").GetComponent<Button>();
+        restartBtn.onClick.AddListener(OnClickBtn);
     }
 
+    private void OnClickBtn()
+    {
+        _count++;
+        var text = this.transform.Find("Text").GetComponent<Text>();
+        text.text = $"english : {_count}";
+
+        var text2 = this.transform.Find("Text2").GetComponent<Text>();
+        text2.text = $"中文 : {_count}";
+    }
     // Update is called once per frame
     void Update()
     {
